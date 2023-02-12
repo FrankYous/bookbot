f5bccab1cf308176cd67e97cf3828ca554d014d4
FrankYous <f_s_yousefi@yahoo.com>
2023-02-12 11:35:47 +0330

    letters are now returned in list format

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 def word_counter(text):
-    words = text.split() #text is split into a list of separate words 
+    words = text.split() #Text is split into a list of separate words 
     return len(words)
 
 def char_counter(text):
     counter = {}
-    text = text.lower() #text is changed to lower case to avoid duplicates in counting of the letters
+    text = text.lower() #Text is changed to lower case to avoid duplicates in counting of the letters
 
-    for i in range(0,len(text)): #creates a dictionary with all possible characters
+    for i in range(0,len(text)): #Creates a dictionary with all possible characters
         counter[text[i]] = 0
-    for i in range(0,len(text)): #now the same dictionary is filled with the counted numbers
+    for i in range(0,len(text)): #Now the same dictionary is filled with the counted numbers
         counter[text[i]] += 1
     return counter
 
-def just_alphabet (dict):
+def just_alphabet (dict): #The function removes all non alphabetical characters
     alph_dict ={}
     for char in dict:
-        if char.isalpha():
+        if char.isalpha(): #If the character is in the alphabet,it will be added to the new dictionary
             alph_dict[char] = dict[char]
     return alph_dict
+
+def dict_to_list(dict_letters): #The dictionary is now transformed into a list of tuples
+    list_letters =[]
+    for letter in dict_letters:
+        list_letters.append([letter,dict_letters[letter]])
+    return list_letters   
+
+def sorter (letters):
+    pass
 
 def main():
     with open("books/frankenstein.txt") as f:
         text = f.read() 
-        print(f"number of words in the book Frankenstein: {word_counter(text)}") #words are counted 
-        characters = char_counter(text)
+
+        print(f"number of words in the book Frankenstein: {word_counter(text)}") #Words are counted 
+        characters = char_counter(text) #The characters are separated into a dictionary
+        alphabet = just_alphabet(characters) #Non letter characters are removed
+        letters = dict_to_list(alphabet) #Dictionary is transformed into a list of tuples
         print("numbers of letters are:")
-        print (characters)
-        alphabet = just_alphabet(characters)
-        print (alphabet)
+        print (letters)
 
 main()
