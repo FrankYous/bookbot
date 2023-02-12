48b477472a4bd653ca99862bc3ee85eef13e0b9f
FrankYous <f_s_yousefi@yahoo.com>
2023-02-12 10:24:16 +0330

    added the function to separate only the letters

--- main.py
+++ main.py
@@ -12,11 +12,21 @@ def char_counter(text):
         counter[text[i]] += 1
     return counter
 
+def just_alphabet (dict):
+    alph_dict ={}
+    for char in dict:
+        if char.isalpha():
+            alph_dict[char] = dict[char]
+    return alph_dict
+
 def main():
     with open("books/frankenstein.txt") as f:
         text = f.read() 
         print(f"number of words in the book Frankenstein: {word_counter(text)}") #words are counted 
+        characters = char_counter(text)
         print("numbers of letters are:")
-        print (char_counter(text))
+        print (characters)
+        alphabet = just_alphabet(characters)
+        print (alphabet)
 
 main()
